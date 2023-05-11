1604cac40cf89e572bc061733b4d0863b3edeb9a
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-05-11 13:17:14 +0300

    server.log

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const fs = require('fs/promises');
+const moment = require('moment')
 
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
+
+app.use(async (req, res, next) => {
+  const { method, url } = req;
+  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
+  await fs.appendFile(".public/server.log", `\n${method} ${url} ${date}`);
+  next();
+})
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
