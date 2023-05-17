af11ef6a73ea57b100c73b977b91d61588bb1d12
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-05-17 16:26:45 +0300

    process.env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const app = require('./app')
-const DB_HOST = "mongodb+srv://Murlykin:rjhb7777@cluster0.0tv83ky.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
+
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000);
